5d469704dd276a1787a0808c3729efcc2370fff3
csseifms <csseifms@gmail.com>
2022-02-11 12:51:05 +0200

    consuming http request

--- hello.go
+++ hello.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 type footballCoach struct{}
 type tennisCoach struct{}
@@ -25,4 +30,12 @@ func main() {
 	printInstructions(f)
 	printInstructions(t)
 
+	response, err := http.Get("http://www.google.com")
+	fmt.Printf("%+v , %+v", response, err)
+
+	// bs := make([]byte, 99999)
+	// response.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, response.Body)
 }
