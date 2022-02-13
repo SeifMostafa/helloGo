dcd78fb98ce794870bd1d012527578d75f2c42ca
csseifms <csseifms@gmail.com>
2022-02-13 14:54:12 +0200

    adding routines,, repeating routines

--- hello.go
+++ hello.go
@@ -8,22 +8,25 @@ import (
 func main() {
 	urls := []string{
 		"http://www.google.com",
+		"http://www.musala.com",
 	}
 	c := make(chan string)
 	for _, url := range urls {
 		go checkURL(url, c)
 	}
-	fmt.Println(<-c)
+	for {
+		go checkURL(<-c, c)
+	}
 }
 func checkURL(url string, c chan string) {
 	_, err := http.Get(url)
 	if err != nil {
-		fmt.Println("ERROR!", err)
-		c <- "error"
+		fmt.Println("ERROR")
+		c <- url
 		return
 	}
 	//io.Copy(os.Stdout, response.Body)
-	fmt.Println("OK.")
-	c <- "ok"
+	fmt.Println("OK")
+	c <- url
 
 }
